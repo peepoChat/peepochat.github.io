c26a8927a127e25eb1b0338aa1997732f4a6b9cb
Creamket_ <66436267+Creamket@users.noreply.github.com>
2020-09-27 12:32:56 +0300

    AutoScroll & pasta trigger

--- js/script.js
+++ js/script.js
@@ -28,39 +28,99 @@ function getRandomInt(max) {
 
 const nicknameColors = ['#ff0000', '#3172ff', '#00dd0e', '#b22222', '#ff7f50', '#9acd32', '#ff4500', '#2e8b57', '#daa520', '#d2691e', '#5f9ea0', '#1e90ff', '#ff69b4', '#8a2be2', '#00ff7f']
 
+// const getRuMessage = function(n){
+//   const a = getRandomInt(n)
+//   if (a < 5){
+//     const message = rupastas[getRandomInt(rupastas.length)]
+//     return message
+//   }
+//   else if(a === 6){
+//     const message = ascii[getRandomInt(ascii.length)]
+//     return message
+//   }
+//   else{
+//     const message = rumessages[getRandomInt(rumessages.length)]
+//     return message
+//   }
+//   console.log(message)
+//   return message
+// }
+
+// const getEnMessage = function(n){
+//   const a = getRandomInt(n)
+//   if (a < 5){
+//     const message = enpastas[getRandomInt(enpastas.length)]
+//     return message
+//   }
+//   else if(a === 6){
+//     const message = ascii[getRandomInt(ascii.length)]
+//     return message
+//   }
+//   else{
+//     const message = enmessages[getRandomInt(enmessages.length)]
+//     return message
+//   }
+//   console.log(message)
+//   return message
+// }
+
 const getRuMessage = function(n){
   const a = getRandomInt(n)
+  let tmessage = {}
+  let tclass
+  let message
   if (a < 5){
-    const message = rupastas[getRandomInt(rupastas.length)]
-    return message
+    tmessage = {
+      message: rupastas[getRandomInt(rupastas.length)],
+      isPasta: true
+    }
   }
   else if(a === 6){
-    const message = ascii[getRandomInt(ascii.length)]
-    return message
+    tmessage = {
+      message: ascii[getRandomInt(ascii.length)],
+      isPasta: true
+    }
   }
   else{
-    const message = rumessages[getRandomInt(rumessages.length)]
-    return message
+    tmessage = {
+      message: rumessages[getRandomInt(rumessages.length)],
+      isPasta: false
+    }
   }
-  console.log(message)
+  if (tmessage.isPasta == true){
+    tclass = 'trigger'
+  } 
+  message = `<div class="message ${tclass}"><b style="color:${nicknameColors[getRandomInt(15)]}"> ${ruusers[getRandomInt(383)]}</b>: ${tmessage.message}</div>`
   return message
 }
 
 const getEnMessage = function(n){
   const a = getRandomInt(n)
+  let tmessage = {}
+  let tclass
+  let message
   if (a < 5){
-    const message = enpastas[getRandomInt(enpastas.length)]
-    return message
+    tmessage = {
+      message: enpastas[getRandomInt(enpastas.length)],
+      isPasta: true
+    }
   }
   else if(a === 6){
-    const message = ascii[getRandomInt(ascii.length)]
-    return message
+    tmessage = {
+      message: ascii[getRandomInt(ascii.length)],
+      isPasta: true
+    }
   }
   else{
-    const message = enmessages[getRandomInt(enmessages.length)]
-    return message
+    tmessage = {
+      message: enmessages[getRandomInt(enmessages.length)],
+      isPasta: false
+    }
   }
-  console.log(message)
+  if (tmessage.isPasta == true){
+    tclass = 'trigger'
+  } 
+  message = `<div class="message ${tclass}"><b style="color:${nicknameColors[getRandomInt(15)]}"> ${enusers[getRandomInt(383)]}</b>: ${tmessage.message}</div>`
   return message
 }
 
@@ -70,7 +130,7 @@ function createRuMessage(){
   message.insertAdjacentHTML('afterbegin', `                                                                 
                             <tr>
                               <td>
-                                <div class="message"><b style="color:${nicknameColors[getRandomInt(15)]}"> ${ruusers[getRandomInt(383)]}</b>: ${stringPasring(getRuMessage(100), ru_emotes)}</div>
+                                ${stringPasring(getRuMessage(100), ru_emotes)}
                               </td>
                             </tr>
   `)
@@ -78,13 +138,14 @@ function createRuMessage(){
   // return message
 }
 
+
 function createEnMessage(){
   $("tbody :nth-last-child(100)").remove()
   const message = document.createElement('tr')
   message.insertAdjacentHTML('afterbegin', `                                                                 
                             <tr>
                               <td>
-                                <div class="message"><b style="color:${nicknameColors[getRandomInt(15)]}"> ${enusers[getRandomInt(383)]}</b>: ${stringPasring(getEnMessage(100), en_emotes)}</div>
+                              ${stringPasring(getEnMessage(100), en_emotes)}
                               </td>
                             </tr>
   `)
@@ -125,7 +186,21 @@ $(document).ready(function(){
 function stringPasring(string, map) {
   for (let key of map.keys()) {
     let parsed = new RegExp(key, "g");
-    string = string.replace(parsed, map.get(key));
+    string = string.replace(parsed, ` ${map.get(key)} `);
   }
   return string;
 }
+
+$(document).ready(function(){
+  $('.ru,.en').hover(
+    function() {
+      clearInterval(shouldScroll)
+    }, function() { 
+      shouldScroll = setInterval(() => {
+        $('.ru')[0].scrollTop = $('.ru')[0].scrollHeight
+        $('.en')[0].scrollTop = $('.en')[0].scrollHeight
+        console.log("Я ГЕЙ")
+      }, 1);
+    })
+  }
+)
